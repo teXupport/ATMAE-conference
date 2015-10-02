fcc3fc1e4da98b1fb9dae67e5fb17c17d1a12849
teXupport <dtwink12@siu.edu>
2015-10-01 20:06:38 -0500

    Update server-pi.py

--- server-pi.py
+++ server-pi.py
@@ -1,7 +1,7 @@
 import socket
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5010
+TCP_PORT = 5012
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
    
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
