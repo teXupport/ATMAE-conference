9f7dfd46a3ca8d499ec8d29e8d0176ebc61baae9
BTwizzler <brenden.tisler@yahoo.com>
2015-10-15 18:49:25 -0500

    Update client-pc.py
    Keyboard control configured

--- client-pc.py
+++ client-pc.py
@@ -1,46 +1,73 @@
-import socket
+import pygame, sys, math, random, socket
+from pygame.locals import *
 
+pygame.init()
+screen = pygame.display.set_mode((800,400))
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5010
+TCP_PORT = 5018
 BUFFER_SIZE = 1024
-MESSAGE = "exit"
+MESSAGE = "w"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 while 1:
-    command = raw_input("Data: ")
-    if (command == "w"):        ## Forward
-        MESSAGE = "LR"
-    elif (command == "s"):   ## Back
-        MESSAGE = "lr"
-    elif (command == "a"):    ## Left
-        MESSAGE = "lR"
-    elif (command == "d"):   ## Right
-        MESSAGE = "Lr"
-    elif (command == "t"):   ## Autonomous Mode
-        MESSAGE = "auto"
-    elif (command == "g"):  ## Conveyor
-        MESSAGE = "c"
-    elif (command == "b"):    ## Brush
-        MESSAGE = "b"
-    elif (command == "y"):   ## Swing Arm
-        MESSAGE = "y"
-    elif (command == "h"):  ## PickUp Dustpan
-        MESSAGE = "h"
-    elif (command == "n"):    ## Dump and Reset Dustpan
-        MESSAGE = "n"
-    elif ((command == "0") or (command == "1") or (command == "2") or (command == "3") or (command == "4") or (command == "5") or (command == "6") or (command == "7") or (command == "8") or (command == "9")):
-        MESSAGE = command       # Motor voltage setting
-    elif (command == "exit"):
-        MESSAGE = "exit"
-    elif (command == "kill"):
-        MESSAGE = "kill"
-    else:
-        MESSAGE = "w"
-    s.send(MESSAGE)
-    if (MESSAGE == "exit" or MESSAGE == "kill"):
+    MESSAGE = "w"
+    for event in pygame.event.get():
+        
+        if (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_w):        ## Forward
+            MESSAGE = "LR"
+        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_s):   ## Back
+            MESSAGE = "lr"
+        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_a):    ## Left
+            MESSAGE = "lR"
+        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_d):   ## Right
+            MESSAGE = "Lr"
+        elif (event.type == KEYUP and event.key == K_t):   ## Autonomous Mode
+            MESSAGE = "auto"
+        elif (event.type == KEYUP and event.key == K_g):  ## Conveyor
+            MESSAGE = "c"
+        elif (event.type == KEYUP and event.key == K_b):    ## Brush
+            MESSAGE = "b"
+        elif (event.type == KEYUP and event.key == K_y):   ## Swing Arm
+            MESSAGE = "y"
+        elif (event.type == KEYUP and event.key == K_h):  ## PickUp Dustpan
+            MESSAGE = "h"
+        elif (event.type == KEYUP and event.key == K_n):    ## Dump and Reset Dustpan
+            MESSAGE = "n"
+        # Motor voltage setting
+        elif (event.type == KEYUP):
+            if (event.key == K_0):
+                MESSAGE = "0"
+            elif (event.key == K_1):
+                MESSAGE = "1"
+            elif (event.key == K_2):
+                MESSAGE = "2"
+            elif (event.key == K_3):
+                MESSAGE = "3"
+            elif (event.key == K_4):
+                MESSAGE = "4"
+            elif (event.key == K_5):
+                MESSAGE = "5"
+            elif (event.key == K_6):
+                MESSAGE = "6"
+            elif (event.key == K_7):
+                MESSAGE = "7"
+            elif (event.key == K_8):
+                MESSAGE = "8"
+            elif (event.key == K_9):
+                MESSAGE = "9"
+        elif (event.type == pygame.QUIT):
+            MESSAGE = "kill"
+        else:
+            MESSAGE = "w"
+    
+    if ((MESSAGE != "w") and (MESSAGE != "kill")):
+        print MESSAGE
+        s.send(MESSAGE)
+    if (MESSAGE == "kill"):
+        s.send("kill")
+        pygame.quit()
+        sys.exit()
         break
-    data = s.recv(BUFFER_SIZE)
-    print "received data:", data
 
 s.close()
