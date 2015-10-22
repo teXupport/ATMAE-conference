fe85c0f3362e68a4e798664b6590256b6470751a
teXupport <dtwink12@siu.edu>
2015-10-22 17:27:57 -0500

    Update client-pc.py

--- client-pc.py
+++ client-pc.py
@@ -3,44 +3,43 @@ from pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5018
+TCP_IP = '192.168.1.106'
+TCP_PORT = 5011
 BUFFER_SIZE = 1024
 MESSAGE = "w"
 keyDown = 0
 lastData = ""
-arduino = serial.Serial('COM14',115200,timeout = .1)
+arduino = serial.Serial('COM4',115200,timeout = .1)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 while 1:
     MESSAGE = "w"
     for event in pygame.event.get():
-        
         if (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_w and not keydown):        ## Forward
             MESSAGE = "LR"
-			if (not keyDown):
-				keyDown = 1
-			else:
-				keyDown = 0
+            if (not keyDown):
+                keyDown = 1
+            else:
+                keyDown = 0
         elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_s and not keydown):   ## Back
             MESSAGE = "lr"
-			if (not keyDown):
-				keyDown = 1
-			else:
-				keyDown = 0
+            if (not keyDown):
+                keyDown = 1
+            else:
+                keyDown = 0
         elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_a and not keydown):    ## Left
             MESSAGE = "lR"
-			if (not keyDown):
-				keyDown = 1
-			else:
-				keyDown = 0
+            if (not keyDown):
+                keyDown = 1
+            else:
+                keyDown = 0
         elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_d and not keydown):   ## Right
             MESSAGE = "Lr"
-			if (not keyDown):
-				keyDown = 1
-			else:
-				keyDown = 0
+            if (not keyDown):
+                keyDown = 1
+            else:
+                keyDown = 0
         elif (event.type == KEYUP and event.key == K_t):   ## Autonomous Mode
             MESSAGE = "auto"
         elif (event.type == KEYUP and event.key == K_g):  ## Conveyor
@@ -53,30 +52,30 @@ while 1:
             MESSAGE = "h"
         elif (event.type == KEYUP and event.key == K_n):    ## Dump and Reset Dustpan
             MESSAGE = "n"
-		elif (event.type == KEYUP and event.key == K_u):    ## Dump and Reset Dustpan
+        elif (event.type == KEYUP and event.key == K_u):    ## Dump and Reset Dustpan
             flag = True
-			s.send("u")
-			while flag:
-				for event in pygame.event.get():
-					if (event.type == KEYUP and event.key == K_u):
-						flag = False
-						s.send("u")
-					else:
-						#read serial stuff
-						lastData = data
-						data = arduino.readline()
-						#send data: format(007127127127\n)
-						if (data == "\n") or (data == lastData):
-							continue
-						else:
-							s.send(data)
+            s.send("u")
+            while flag:
+                for event in pygame.event.get():
+                    if (event.type == KEYUP and event.key == K_u):
+                        flag = False
+                        s.send("u")
+                    else:
+                        #read serial stuff
+                        lastData = data
+                        data = arduino.readline()
+                        #send data: format(007127127127\n)
+                        if (data == "\n") or (data == lastData):
+                            continue
+                        else:
+                            s.send(data)
         elif (event.type == pygame.QUIT):
             MESSAGE = "kill"
         else:
             MESSAGE = "w"
-    
+
     if ((MESSAGE != "w") and (MESSAGE != "kill")):
-        print MESSAGE
+        print(MESSAGE)
         s.send(MESSAGE)
     if (MESSAGE == "kill"):
         s.send("kill")
