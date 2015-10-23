5e1c98acc453f9abbc6c69e0149bbb22787a083a
Tyler Winkleman <dtwink12@siu.edu>
2015-10-22 21:20:48 -0500

    Updated python scripts
    The code now supports driving and arm control. The code also has
    autonomous mode. #TODO Test and adjust values for line following.

--- client-pc.py
+++ client-pc.py
@@ -3,46 +3,49 @@ from pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5018
+TCP_IP = '192.168.1.106'
+TCP_PORT = 5016
 BUFFER_SIZE = 1024
 MESSAGE = "w"
 keyDown = 0
-lastData = ""
-arduino = serial.Serial('COM14',115200,timeout = .1)
+data = ""
+
+##arduino = serial.Serial('/dev/ttyACM0',115200,timeout = .1)  # for Raspberry Pi
+arduino = serial.Serial('COM4',115200,timeout = .1)  # for Windows computer
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+
 while 1:
     MESSAGE = "w"
     for event in pygame.event.get():
         
-        if (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_w and not keydown):        ## Forward
+        if (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_w and not keyDown):        ## Forward
             MESSAGE = "LR"
-			if (not keyDown):
-				keyDown = 1
-			else:
-				keyDown = 0
-        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_s and not keydown):   ## Back
+            if (not keyDown):
+                    keyDown = 1
+            else:
+                    keyDown = 0
+        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_s and not keyDown):   ## Back
             MESSAGE = "lr"
-			if (not keyDown):
-				keyDown = 1
-			else:
-				keyDown = 0
-        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_a and not keydown):    ## Left
+            if (not keyDown):
+                    keyDown = 1
+            else:
+                    keyDown = 0
+        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_a and not keyDown):    ## Left
             MESSAGE = "lR"
-			if (not keyDown):
-				keyDown = 1
-			else:
-				keyDown = 0
-        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_d and not keydown):   ## Right
+            if (not keyDown):
+                    keyDown = 1
+            else:
+                    keyDown = 0
+        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_d and not keyDown):   ## Right
             MESSAGE = "Lr"
-			if (not keyDown):
-				keyDown = 1
-			else:
-				keyDown = 0
+            if (not keyDown):
+                    keyDown = 1
+            else:
+                    keyDown = 0
         elif (event.type == KEYUP and event.key == K_t):   ## Autonomous Mode
-            MESSAGE = "auto"
+            MESSAGE = "t"
         elif (event.type == KEYUP and event.key == K_g):  ## Conveyor
             MESSAGE = "c"
         elif (event.type == KEYUP and event.key == K_b):    ## Brush
@@ -53,33 +56,36 @@ while 1:
             MESSAGE = "h"
         elif (event.type == KEYUP and event.key == K_n):    ## Dump and Reset Dustpan
             MESSAGE = "n"
-		elif (event.type == KEYUP and event.key == K_u):    ## Dump and Reset Dustpan
+        elif (event.type == KEYUP and event.key == K_u):    ## Dump and Reset Dustpan
             flag = True
-			s.send("u")
-			while flag:
-				for event in pygame.event.get():
-					if (event.type == KEYUP and event.key == K_u):
-						flag = False
-						s.send("u")
-					else:
-						#read serial stuff
-						lastData = data
-						data = arduino.readline()
-						#send data: format(007127127127\n)
-						if (data == "\n") or (data == lastData):
-							continue
-						else:
-							s.send(data)
+            MESSAGE = "u"
+            s.send(MESSAGE.encode())
+            while flag:
+                    for event in pygame.event.get():
+                            if (event.type == KEYUP and event.key == K_u):
+                                    flag = False
+                                    MESSAGE = "u"
+                                    s.send(MESSAGE.encode())
+
+                    if flag:
+                        #read serial stuff
+                        data = arduino.readline()
+                        #send data: format(007127127127\n)
+                      
+                        if (data == "\n"):
+                            continue
+                        else:
+                            s.send(data)
         elif (event.type == pygame.QUIT):
-            MESSAGE = "kill"
+            MESSAGE = "k"
         else:
             MESSAGE = "w"
     
-    if ((MESSAGE != "w") and (MESSAGE != "kill")):
-        print MESSAGE
-        s.send(MESSAGE)
-    if (MESSAGE == "kill"):
-        s.send("kill")
+    if ((MESSAGE != "w") and (MESSAGE != "k")):
+        print(MESSAGE)
+        s.send(MESSAGE.encode())
+    if (MESSAGE == "k"):
+        s.send(MESSAGE.encode())
         pygame.quit()
         sys.exit()
         break
