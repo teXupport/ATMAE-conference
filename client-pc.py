b0bc68465a3aa0cd532d63b8c1beb7cd4d09461c
Tyler Winkleman <dtwink12@siu.edu>
2015-10-26 22:15:09 -0500

    Fixed flags

--- client-pc.py
+++ client-pc.py
@@ -3,47 +3,51 @@ from pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
-TCP_IP = '192.168.1.106'
-TCP_PORT = 5016
-BUFFER_SIZE = 1024
+TCP_IP = '192.168.1.127'
+TCP_PORT = 5022
+BUFFER_SIZE = 256
 MESSAGE = "w"
-keyDown = 0
+drive = ""
 data = ""
-##arduino = serial.Serial('/dev/ttyACM0',115200,timeout = .1)
-arduino = serial.Serial('COM4',115200,timeout = .1)
+##arduino = serial.Serial('/dev/ttyACM0', 115200, timeout = .1)  # Raspberry Pi
+arduino = serial.Serial('COM4', 115200, timeout = .1)  # Windows computer
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+
 while 1:
     MESSAGE = "w"
-    for event in pygame.event.get():
-        
-        if (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_w and not keyDown):        ## Forward
-            MESSAGE = "LR"
-            if (not keyDown):
-                    keyDown = 1
-            else:
-                    keyDown = 0
-        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_s and not keyDown):   ## Back
-            MESSAGE = "lr"
-            if (not keyDown):
-                    keyDown = 1
-            else:
-                    keyDown = 0
-        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_a and not keyDown):    ## Left
-            MESSAGE = "lR"
-            if (not keyDown):
-                    keyDown = 1
-            else:
-                    keyDown = 0
-        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_d and not keyDown):   ## Right
-            MESSAGE = "Lr"
-            if (not keyDown):
-                    keyDown = 1
-            else:
-                    keyDown = 0
+    for event in pygame.event.get():     
+        if (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_w):        ## Forward
+            if (drive == ""):
+                    drive = "w"
+                    MESSAGE = "LR"
+            elif (drive == "w"):
+                    drive = ""
+                    MESSAGE = "LR"
+        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_s):   ## Back
+            if (drive == ""):
+                    drive = "s"
+                    MESSAGE = "lr"
+            elif (drive == "s"):
+                    drive = ""
+                    MESSAGE = "lr"
+        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_a):    ## Left
+            if (drive == ""):
+                    drive = "a"
+                    MESSAGE = "lR"
+            elif (drive == "a"):
+                    drive = ""
+                    MESSAGE = "lR"
+        elif (((event.type == KEYDOWN) or event.type == KEYUP) and event.key == K_d):   ## Right
+            if (drive == ""):
+                    drive = "d"
+                    MESSAGE = "Lr"
+            elif (drive == "d"):
+                    drive = ""
+                    MESSAGE = "Lr"
         elif (event.type == KEYUP and event.key == K_t):   ## Autonomous Mode
-            MESSAGE = "auto"
+            MESSAGE = "t"
         elif (event.type == KEYUP and event.key == K_g):  ## Conveyor
             MESSAGE = "c"
         elif (event.type == KEYUP and event.key == K_b):    ## Brush
@@ -66,7 +70,6 @@ while 1:
                                     s.send(MESSAGE.encode())
 
                     if flag:
-                        #read serial stuff
                         data = arduino.readline()
                         #send data: format(007127127127\n)
                       
@@ -85,7 +88,7 @@ while 1:
     if (MESSAGE == "k"):
         s.send(MESSAGE.encode())
         pygame.quit()
-        sys.exit()
         break
 
 s.close()
+sys.exit()
